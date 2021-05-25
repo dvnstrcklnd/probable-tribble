e8a09eedeae0945d9b16c03e5a1ac4c93f0944ec
Devin Strickland <strcklnd@uw.edu>
2021-05-25 00:53:07 +0000

    working args

--- probable_tribble.py
+++ probable_tribble.py
@@ -1,5 +1,53 @@
+"""
+Tools for processing lists and strings
+"""
+__author__ = "Devin Strickland"
+__maintainer__ = "Devin Strickland"
+__email__ = "dvn.strcklnd@gmail.com"
+__copyright__ = "Copyright 2021, Devin Strickland"
+
+import sys
+import argparse
+
 def main():
-    return "Hello World"
+    parser = get_argument_parser()
+    args = parser.parse_args()
+    try:
+        args.func(args)
+    except AttributeError:
+        parser.print_help(sys.stderr)
+
+def get_argument_parser():
+    parser = argparse.ArgumentParser(
+        description="Process lists and strings")
+
+    subparsers = parser.add_subparsers(title="subcommands")
+
+    parser_count_over_under = subparsers.add_parser("count-over-under")
+    parser_count_over_under.add_argument('-l', '--list', nargs='+',
+                                         help="the list of integers to be processed")
+    parser_count_over_under.add_argument('-v', '--value',
+                                         help="the value that items are over or under")
+    parser_count_over_under.set_defaults(func=do_count_over_under)
+
+    parser_rotate_string = subparsers.add_parser("rotate-string")
+    parser_rotate_string.add_argument('-s','--string',
+                                      help="the string to rotate")
+    parser_rotate_string.add_argument('-n', '--number',
+                                      help="the number of letters to rotate")
+    parser_rotate_string.set_defaults(func=do_rotate_string)
+
+    return parser
+
+def do_count_over_under(args):
+    print("count_over_under")
+    print(args.list)
+    print(args.value)
+
+def do_rotate_string(args):
+    print("rotate_string")
+    print(args.string)
+    print(args.number)
 
 if __name__ == "__main__":
     main()
